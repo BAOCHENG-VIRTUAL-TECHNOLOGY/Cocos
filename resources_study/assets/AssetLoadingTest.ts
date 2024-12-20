0a753268cdb9aa86ab27d4b28b763e50bc0b76c4
zengbaocheng <1154375864@qq.com>
2024-12-20 17:39:52 +0800

    update

--- resources_study/assets/AssetLoadingTest.ts
+++ resources_study/assets/AssetLoadingTest.ts
@@ -1,5 +1,5 @@
-import { MeshRenderer, SkinnedMeshRenderer, sp } from 'cc';
-import { _decorator, Component, Node, resources, instantiate, Prefab, AnimationClip, Animation, SpriteFrame
+import { MeshRenderer, SkinnedMeshRenderer, sp, TextAsset } from 'cc';
+import { assetManager,ImageAsset,_decorator, Component, Node, resources, instantiate, Prefab, AnimationClip, Animation, SpriteFrame
  ,Sprite,Texture2D, SpriteAtlas,Mesh,Material,Skeleton} from 'cc';
 const { ccclass, property } = _decorator;
 
@@ -25,20 +25,54 @@ export class AssetLoadingTest extends Component {
         //     const frame = atlas.getSpriteFrame("1.png");
         //     sprite.spriteFrame = frame;
         // })
-        resources.load("Monster/monster",Mesh,(err, mesh)=>{
-            this.node.getComponent(MeshRenderer).mesh = mesh;
+        // resources.load("Monster/monster",Mesh,(err, mesh)=>{
+        //     this.node.getComponent(MeshRenderer).mesh = mesh;
+        // })
+        // resources.load("Monster/monster-effect",Material,(err, material)=>{
+        //     this.node.getComponent(MeshRenderer).material = material;
+        // })
+        // resources.load("Monster/Armature",Skeleton,(err, skeleton)=>{
+        //     this.node.getComponent(SkinnedMeshRenderer).skeleton = skeleton;
+        // })
+
+        // resources.loadDir("test_assets",function(err,assets){
+
+        // })
+        // resources.loadDir("test_assets",SpriteFrame,function(err,assets){
+
+        // })
+        resources.preload('test_assets/image/spriteFrame',SpriteFrame);
+        resources.load('test_assets/image/spriteFrame',SpriteFrame,(err,spriteFrame)=>{
+            this.node.getComponent(Sprite).spriteFrame = spriteFrame;
         })
-        resources.load("Monster/monster-effect",Material,(err, material)=>{
-            this.node.getComponent(MeshRenderer).material = material;
+        let remoteUrl = "http://unknown.org/someres.png";
+        assetManager.loadRemote<ImageAsset>(remoteUrl, (err, imageAsset) => {
+            const spriteFrame = new SpriteFrame();
+            const texture = new Texture2D();
+            texture.image = imageAsset;
+            spriteFrame.texture = texture;
         })
-        resources.load("Monster/Armature",Skeleton,(err, skeleton)=>{
-            this.node.getComponent(SkinnedMeshRenderer).skeleton = skeleton;
+        remoteUrl="http://unknown.org/emoji?id=124982374"
+        assetManager.loadRemote<ImageAsset>(remoteUrl,{ext:'.png'},function(err, imageAsset){
+            const spriteFrame = new SpriteFrame();
+            const texture = new Texture2D();
+            texture.image = imageAsset;
+            spriteFrame.texture = texture;
+        })
+        const absolutePath = "/data/some/path/to/image.png"
+        assetManager.loadRemote<ImageAsset>(absolutePath,function(err,imageAsset){
+            const spriteFrame = new SpriteFrame();
+            const texture = new Texture2D();
+            texture.image = imageAsset;
+            spriteFrame.texture = texture;
+        })
+        remoteUrl = "http://unknown.org/sound.mp3";
+        assetManager.loadRemote(remoteUrl, function(err, audioClip) {
+            
         })
 
-        resources.loadDir("test_assets",function(err,assets){
-
-        })
-        resources.loadDir("test_assets",SpriteFrame,function(err,assets){
+        remoteUrl = "http://unknown.org/skill.txt";
+        assetManager.loadRemote(remoteUrl, function(err, textAsset){
 
         })
     }
